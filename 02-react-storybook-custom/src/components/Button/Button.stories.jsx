ed61f8c92e0e661f615fd4a9152671b292bf7c1a
Noah Kim <dongwoo_333@hanmail.net>
2023-02-02 14:35:35 +1100

    feat: wrap <Button /> with <Center /> without decorator

--- 02-react-storybook-custom/src/components/Button/Button.stories.jsx
+++ 02-react-storybook-custom/src/components/Button/Button.stories.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Button from "./Button";
+import Center from "../Center/Center";
 
 export default {
   title: "form/Button",
@@ -9,10 +10,26 @@ export default {
   },
 };
 
-export const Primary = () => <Button variant="primary">Primary</Button>;
-export const Secondary = () => <Button variant="secondary">Secondary</Button>;
-export const Success = () => <Button variant="success">Success</Button>;
-export const Danger = () => <Button variant="danger">Danger</Button>;
+export const Primary = () => (
+  <Center>
+    <Button variant="primary">Primary</Button>
+  </Center>
+);
+export const Secondary = () => (
+  <Center>
+    <Button variant="secondary">Secondary</Button>
+  </Center>
+);
+export const Success = () => (
+  <Center>
+    <Button variant="success">Success</Button>
+  </Center>
+);
+export const Danger = () => (
+  <Center>
+    <Button variant="danger">Danger</Button>
+  </Center>
+);
 
 const Template = (args) => <Button {...args} />;
 
