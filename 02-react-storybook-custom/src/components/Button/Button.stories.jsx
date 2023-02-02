b3988619e5220921fbe6a8d03081146202940da5
Noah Kim <dongwoo_333@hanmail.net>
2023-02-02 14:37:56 +1100

    feat: use decorator in component level

--- 02-react-storybook-custom/src/components/Button/Button.stories.jsx
+++ 02-react-storybook-custom/src/components/Button/Button.stories.jsx
@@ -5,31 +5,16 @@ import Center from "../Center/Center";
 export default {
   title: "form/Button",
   component: Button,
+  decorators: [(story) => <Center>{story()}</Center>],
   args: {
     children: "Button",
   },
 };
 
-export const Primary = () => (
-  <Center>
-    <Button variant="primary">Primary</Button>
-  </Center>
-);
-export const Secondary = () => (
-  <Center>
-    <Button variant="secondary">Secondary</Button>
-  </Center>
-);
-export const Success = () => (
-  <Center>
-    <Button variant="success">Success</Button>
-  </Center>
-);
-export const Danger = () => (
-  <Center>
-    <Button variant="danger">Danger</Button>
-  </Center>
-);
+export const Primary = () => <Button variant="primary">Primary</Button>;
+export const Secondary = () => <Button variant="secondary">Secondary</Button>;
+export const Success = () => <Button variant="success">Success</Button>;
+export const Danger = () => <Button variant="danger">Danger</Button>;
 
 const Template = (args) => <Button {...args} />;
 
