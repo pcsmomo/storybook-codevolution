a0c78ca306d553426804d74c329839d8e4791fec
Noah Kim <dongwoo_333@hanmail.net>
2023-02-02 15:14:38 +1100

    feat: global decorators in storybook v6

--- 02-react-storybook-custom/.storybook/preview.js
+++ 02-react-storybook-custom/.storybook/preview.js
@@ -1,9 +1,6 @@
 import React from "react";
-import { addDecorator } from "@storybook/react";
+// import { addDecorator } from "@storybook/react";
 import Center from "../src/components/Center/Center";
-
-// Global decorator
-addDecorator((story) => <Center>{story()}</Center>);
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -20,3 +17,15 @@ export const parameters = {
         : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
   },
 };
+
+// Global decorator
+export const decorators = [
+  (Story) => (
+    <Center>
+      <Story />
+    </Center>
+  ),
+];
+
+// Global decorator in an alternative way
+// addDecorator((story) => <Center>{story()}</Center>);
