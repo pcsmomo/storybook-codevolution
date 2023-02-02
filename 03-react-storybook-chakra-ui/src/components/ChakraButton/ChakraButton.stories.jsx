af600be7f2d41426e73cdf461d1b00e27bdfa903
Noah Kim <dongwoo_333@hanmail.net>
2023-02-02 15:31:55 +1100

    feat: basic understanding about addons

--- 03-react-storybook-chakra-ui/src/components/ChakraButton/ChakraButton.stories.jsx
+++ 03-react-storybook-chakra-ui/src/components/ChakraButton/ChakraButton.stories.jsx
@@ -4,7 +4,23 @@ import { Button } from "@chakra-ui/react";
 export default {
   title: "Chakra/Button",
   component: Button,
+  argTypes: {
+    colorScheme: { control: "text" },
+    children: { control: "text" },
+    onClick: { action: "clicked" },
+  },
 };
 
-export const Success = () => <Button colorScheme="green">Success</Button>;
-export const Danger = () => <Button colorScheme="red">Danger</Button>;
+const Template = (args) => <Button {...args} />;
+
+export const Success = Template.bind();
+Success.args = {
+  colorScheme: "green",
+  children: "Success",
+};
+
+export const Danger = Template.bind();
+Danger.args = {
+  colorScheme: "red",
+  children: "Danger",
+};
